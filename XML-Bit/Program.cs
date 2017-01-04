b81e59c2d7f2a9e60a165b9d1e5551c0922a9f15
petrikuronen <p.kuronen@outlook.com>
2017-01-04 18:42:18 +0100

    Ändrat lite till

--- XML-Bit/Program.cs
+++ XML-Bit/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
+using System.Xml.Schema;
+using System.Xml.XPath;
 
 namespace XML_Bit
 {
@@ -11,13 +13,13 @@ namespace XML_Bit
     {
         static void Main(string[] args)
         {
-            machine[] mm = new machine[5];//("192.168.0.1", 10, 10);
-            for(int i = 0; i < 5; i++)
+            machine[] mm = new machine[1];//("192.168.0.1", 10, 10);
+            for(int i = 0; i < 1; i++)
                 mm[i] = new machine("192.168.0.11"+i.ToString(), 3, 3);
             //mm[1] = new machine("192.168.0.22", 3, 3);
             Console.WriteLine(mm[0].SetPort(1, true));
             mm[0].SetCounter(0, 23003);
-            mm[1].SetCounter(1, 550022);
+            //mm[1].SetCounter(1, 550022);
             Console.WriteLine("machine id: {0}, ports: {1}", mm[0].id, mm[0].ports);
             Console.WriteLine("machine id: {0}, counter: {1}", mm[0].id, mm[0].GetCounter(1));
             mm[0].CountUp(1);
@@ -29,57 +31,112 @@ namespace XML_Bit
             Console.WriteLine("Antal maskiner {0}", mm.Count());
 
             XmlDocument doc = new XmlDocument();
-            //XmlDeclaration xmlDeclaration = doc.CreateXmlDeclaration("1.0", "UTF-8", null);
-            XmlElement ele = doc.CreateElement("root"); ;
-                              
-            
+            XmlDocument doc1 = new XmlDocument();
+            /**/
+            XmlNode docNode = doc.CreateXmlDeclaration("1.0", "UTF-8", null);
+            doc.AppendChild(docNode);
+
+            XmlNode rootNode = doc.CreateElement("root");
+            doc.AppendChild(rootNode);
+            /**/
+
+
             // Create a new element.  
-            for(int i = 0; i < mm.Length; i++)
-                ele.AppendChild(AddNewMachine(mm[i].id, mm[i].getAllPorts(), mm[i].getAllCounters(), doc));
+            for (int i = 0; i < mm.Length; i++)
+                AddNewMachine(mm[i].id, mm[i].getAllPorts(), mm[i].getAllCounters(), doc, rootNode);
+
+             XmlWriterSettings setting = new XmlWriterSettings();
+             setting.Indent = true;
+             setting.IndentChars = ("\t");
+             setting.OmitXmlDeclaration = true;
+            setting.ConformanceLevel = ConformanceLevel.Fragment;
+
+             // Create the XmlWriter object and write some content.
+             XmlWriter write = null;
+             write = XmlWriter.Create("Machine.xml", setting);
             
-            Console.Write(ele.OuterXml.ToString());
-            Console.ReadLine();
+            doc.WriteContentTo(write);
+            write.Close();
+
+            Console.Write(doc.OuterXml.ToString());
+
+            /******/
+            try
+            {
+                XmlReaderSettings settings = new XmlReaderSettings();
+                settings.Schemas.Add("http://tempuri.org/MachineSchema.xsd", "Machine.xsd");
+                settings.ValidationType = ValidationType.Schema;
+
+                XmlReader reader = XmlReader.Create("Machine.xml", settings);
+                XmlDocument document = new XmlDocument();
+                document.Load(reader);
+
+                ValidationEventHandler eventHandler = new ValidationEventHandler(ValidationEventHandler);
+
+                // the following call to Validate succeeds.
+                document.Validate(eventHandler);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Häpp");
+                Console.WriteLine(ex.Message);
+                
+            }
+        
+
+       
+    
+    /******/
+    Console.ReadLine();
         }
 
 
-       static XmlElement AddNewMachine(string id, int[] port, int[] counter, XmlDocument doc)
+       static XmlNode AddNewMachine(string id, int[] port, int[] counter, XmlDocument doc, XmlNode rootNode) //XmlElement
         {
-            XmlElement machineElement = doc.CreateElement("source");
-            
-            XmlAttribute attribute = doc.CreateAttribute("id");
-            attribute.Value = id;
-            
-            machineElement.Attributes.Append(attribute);
+            /****/
+            XmlNode sourceNode = doc.CreateElement("source");
+            XmlAttribute sourceAttribute = doc.CreateAttribute("id");
+            sourceAttribute.Value = id;
+            sourceNode.Attributes.Append(sourceAttribute);
+            rootNode.AppendChild(sourceNode);
 
-            XmlElement portsElement = doc.CreateElement("inputs");
-            machineElement.AppendChild(portsElement);
-            
+            XmlNode inputsNode = doc.CreateElement("inputs");
+            sourceNode.AppendChild(inputsNode);
+
             // lägg till status på portarna
             for (int i = 0, j = 1; i < port.Length; i++, j++)
             {
-                XmlNode portElement = doc.CreateElement("input");
+                XmlNode inputNode = doc.CreateElement("input");
                 XmlAttribute portAttribute = doc.CreateAttribute("port");
                 portAttribute.Value = j.ToString();
 
                 XmlElement status = doc.CreateElement("status");
-                portElement.AppendChild(status);
+                inputNode.AppendChild(status);
                 status.InnerText = port[i].ToString();
                 
                 //räknare
-               
                 XmlElement count = doc.CreateElement("counter");
                 count.InnerText = counter[i].ToString();
-                status.AppendChild(count);
+                inputNode.AppendChild(count);
 
-                portElement.Attributes.Append(portAttribute);
-                portsElement.AppendChild(portElement);
+                inputNode.Attributes.Append(portAttribute);
+                inputsNode.AppendChild(inputNode);
                
             }
-            machineElement.AppendChild(portsElement);
-
-
-            return machineElement;// rootElement;
-            ;
+            sourceNode.AppendChild(inputsNode);
+            return doc;
+        }
+        static void ValidationEventHandler(object sender, ValidationEventArgs e)
+        {
+            switch (e.Severity)
+            {
+                case XmlSeverityType.Error:
+                    Console.WriteLine("Error: {0}", e.Message);
+                    break;
+                case XmlSeverityType.Warning:
+                    Console.WriteLine("Warning {0}", e.Message);
+                    break;
+            }
 
         }
     }
